4b14589285f91c350833a82c9f6aaeaafccbd91b
replay9902 <=>
2020-01-22 11:41:23 +0900

    뷰페이지 사진 facbybox 플러그인 적용

--- application/views/layout/header_v.php
+++ application/views/layout/header_v.php
@@ -7,7 +7,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
 <meta name="format-detection" content="telephone=no">
 <script src="<?=BASE_URL?>assets/js/jquery-1.11.1.min.js"></script>
+<script src="<?=BASE_URL?>assets/js/jquery-migrate-1.2.1.min.js"></script>
+<script src="<?=BASE_URL?>assets/js/jquery.mousewheel-3.0.4.pack.js"></script>
+<script src="<?=BASE_URL?>assets/js/jquery.fancybox-1.3.4.pack.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="<?=BASE_URL?>assets/css/jquery.fancybox-1.3.4.css" type="text/css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
--- application/views/view_v.php
+++ application/views/view_v.php
@@ -2,15 +2,12 @@
 <div class="row">
 	<h1 class="font-weight-light text-center text-lg-left mt-4 mb-0"><?=$article->title?></h1>
 </div>
-
 <hr class="mt-2 mb-5">
-	
 <div class="row mt-2 mb-2">
 	<div class="col-md-12 text-right">
 		<a href="<?=BASE_URL?>main/index/<?=$this->uri->segment(5)?>" class="btn btn-secondary">목록</a>
 	</div>
 </div>
-	
 <div class="row">
 	<div class="col-lg-12">
 		<div class="main-card mb-3 card">
@@ -20,9 +17,9 @@
 						<div class="widget-content-wrapper">
 							<div class="widget-content-left">
 								<div class="widget-heading">
-									<b>분류</b>
+									<b>지역</b>
 								</div>
-								<div class="widget-subheading"><?=$article->cate?></div>
+								<div class="widget-subheading"><?=$article->location?></div>
 							</div>
 						</div>
 					</div>
@@ -32,9 +29,9 @@
 						<div class="widget-content-wrapper">
 							<div class="widget-content-left">
 								<div class="widget-heading">
-									<b>지역</b>
+									<b>분류</b>
 								</div>
-								<div class="widget-subheading"><?=$article->location?></div>
+								<div class="widget-subheading"><?=$article->cate?></div>
 							</div>
 						</div>
 					</div>
@@ -57,62 +54,64 @@
 		</div>
 	</div>
 </div>
-	
-	
 <ul class="nav nav-tabs nav-justified mb-4">
 	<li class="nav-item"><a role="tab" class="nav-link active" id="tab-0" data-toggle="tab" href="#tab-content-0"> <span>사진 [ <?=count($attachs)?> ] </span></a></li>
 	<li class="nav-item"><a role="tab" class="nav-link" id="tab-1" data-toggle="tab" href="#tab-content-1"> <span>영상</span></a></li>
 </ul>
-	
 <div class="tab-content">
 	<div class="tab-pane tabs-animation fade active show" id="tab-content-0" role="tabpanel">
-		<div id="carousel_photo" class="carousel slide" data-ride="carousel">
-			<div class="carousel-inner">
-<?php
-$i = 0;
-foreach($attachs as $value) {
-	$src = BASE_URL . "data/" . $this->uri->segment(3) . "/" . $value->attach_name;
-	?>
-				<div class="carousel-item<?=$i == 0 ? " active" : ""?>">
-					<img class="d-block w-100" src="<?=$src?>" alt="First slide">
-				</div>
-<?php
-	$i++;
-}
-?>
+		<div class="row">
+		<?php
+		foreach($attachs as $value) {
+			$src = BASE_URL . "data/" . $this->uri->segment(3) . "/" . $value->attach_name;
+			?>
+			<div class="col-lg-3 col-md-4 col-6">
+				<a href="<?=$src?>" class="photo"> <img class="img-fluid img-thumbnail" src="<?=$src?>" alt="">
+				</a>
 			</div>
-			
-			<a class="carousel-control-prev" href="#carousel_photo" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carousel_photo" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+		<?php
+		}
+		?>
 		</div>
 	</div>
-		
 	<div class="tab-pane tabs-animation fade" id="tab-content-1" role="tabpanel">
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-12">
 			<?php if($article->video != ""){?>
 			<div class="embed-responsive embed-responsive-16by9">
-				<iframe class="embed-responsive-item" src="<?=$article->video?>" allowfullscreen=""></iframe>
-			</div>
-			<?php }else{?>
-				<div class="jumbotron text-center">
-				등록된 영상이 없습니다.
+					<iframe class="embed-responsive-item" src="<?=$article->video?>" allowfullscreen=""></iframe>
 				</div>
+			<?php }else{?>
+				<div class="jumbotron text-center">등록된 영상이 없습니다.</div>
 			<?php }?>
 			</div>
 		</div>
 	</div>
 </div>
-
-
 <div class="row mt-2 mb-2">
 	<div class="col-md-12 text-right">
 		<a href="<?=BASE_URL?>main/index/<?=$this->uri->segment(5)?>" class="btn btn-secondary">목록</a>
 	</div>
 </div>
+<script>
+jQuery(function($){
+	
+	$('.photo').fancybox({
+	
+		padding : 10,
+		imageScale : false,
+		overlayShow : true,
+		overlayOpacity : 0.9,
+		cyclic : true,
+		transitionIn : 'fade',	//elastic, fade, none
+		transitionOut : 'fade',	//elastic, fade, none
+		speedIn : 500,
+		speedOut : 500,
+		zoomOpacity : true,
+		titleShow : true,
+		titleFormat : this.title,
+		titlePosition : 'over'		// outside, inside, over
+	
+	});
+});
+</script>
